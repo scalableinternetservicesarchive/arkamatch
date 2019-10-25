74dbad137a0fcddbf466a921a9aff692b2554595
Yijing Zhou <yijingz1227@gmail.com>
2019-10-25 16:24:13 -0700

    bind and pidfile doesn ot work

--- config/puma.rb
+++ config/puma.rb
@@ -7,9 +7,6 @@
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 
-bind "unix:///var/run/puma/my_app.sock"
-pidfile "/var/run/puma/my_app.sock"
-
 threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
