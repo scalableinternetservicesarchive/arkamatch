8c1961f7a57aa39f0ae5d7a740bbb5482d695091
anurang <anurang@g.ucla.edu>
2019-11-01 00:10:17 -0700

    addded backend app

---
+++ back_end/db/migrate/20191101070910_create_user_preferences.rb
@@ -0,0 +1,10 @@
+class CreateUserPreferences < ActiveRecord::Migration[6.0]
+  def change
+    create_table :user_preferences do |t|
+      t.string :name
+      t.string :interest
+
+      t.timestamps
+    end
+  end
+end
