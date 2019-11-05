4386f5d2f39195f09cd174fed5f9543103ec5578
Yijing Zhou <yijingz1227@gmail.com>
2019-11-04 19:36:39 -0800

    Added a static pages controller following the tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
   get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    root to: 'pages#home'
