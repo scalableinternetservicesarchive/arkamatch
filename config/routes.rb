c54aa4abbba380738d8e53c5a2df7276981455c1
Yijing Zhou <yijingz1227@gmail.com>
2019-11-04 23:27:49 -0800

    Added static home & help pages under route static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
-  get 'hello_world', to: 'hello_world#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-   root to: 'pages#home'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,21 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @title_suffix = "Arkamatch"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@title_suffix}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | #{@title_suffix}"
   end
 
 end
