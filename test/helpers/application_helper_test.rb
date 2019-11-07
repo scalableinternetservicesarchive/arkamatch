77bb0ab642b06ec7c8167ff7d226cbc058359473
Yijing Zhou <yijingz1227@gmail.com>
2019-11-07 00:07:02 -0800

    Preparing for merge with vanilla

--- test/helpers/application_helper_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class ApplicationHelperTest < ActionView::TestCase
-  test "full title helper" do
-    assert_equal full_title_for_each_page,         'Arkamatch'
-    assert_equal full_title_for_each_page("Home"), 'Home | Arkamatch'
-  end
-end
