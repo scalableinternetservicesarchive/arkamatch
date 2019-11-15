2196e0df8b7b54e79a85bacd6d4573fab5c1ef46
Hakan Alpay <hak7alp@gmail.com>
2019-11-15 13:05:41 -0800

    removed user preferences from user table

--- back_end/db/schema.rb
+++ back_end/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_204640) do
+ActiveRecord::Schema.define(version: 2019_11_15_203140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,7 +68,6 @@ ActiveRecord::Schema.define(version: 2019_11_15_204640) do
     t.string "username", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "preferences"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
