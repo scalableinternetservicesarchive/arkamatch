a5c02460abb75fe67ba0229bf9468538771c4add
Yijing Zhou <yijingz1227@gmail.com>
2019-11-15 12:51:28 -0800

    Add in print statement to check request parameters

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+    puts request.body.read
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
