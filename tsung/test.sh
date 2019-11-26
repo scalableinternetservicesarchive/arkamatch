faeec85c61be94c7ed1be11ad632db7a7c09e133
Yijing Zhou <yijingz1227@gmail.com>
2019-11-26 12:52:12 -0800

    Modify test.sh to allow simple testing on cloudformation ec2 instance

--- tsung/test.sh
+++ tsung/test.sh
@@ -2,7 +2,16 @@
 TEST_CSV=test.csv
 TEST_SCRIPT=simple.xml
 WORKING_DIR=$(pwd)/$TEST_CSV
+SERVER_LOC=$1
 
-sed -i '' -e 's|'PATH_PLACEHOLDER'|'"$WORKING_DIR"'|g' $TEST_SCRIPT
+if [ -z "$SERVER_LOC" ]
+then
+    echo "Please provide a server address"
+    exit
+fi
+
+sed -i -e 's|'PATH_PLACEHOLDER'|'"$WORKING_DIR"'|g' $TEST_SCRIPT
+sed -i -e 's|'SERVER_PLACEHOLDER'|'"$SERVER_LOC"'|g' $TEST_SCRIPT
 tsung -f $TEST_SCRIPT start
-sed -i '' -e 's|'"$WORKING_DIR"'|'PATH_PLACEHOLDER'|g' $TEST_SCRIPT
+sed -i -e 's|'"$WORKING_DIR"'|'PATH_PLACEHOLDER'|g' $TEST_SCRIPT
+sed -i -e 's|'"$SERVER_LOC"'|'SERVER_PLACEHOLDER'|g' $TEST_SCRIPT
