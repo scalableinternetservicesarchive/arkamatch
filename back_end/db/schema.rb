22a155d42f15b15caa668e8dd8c659ea465ecbf9
anurang <anurang@g.ucla.edu>
2019-11-27 21:40:21 -0800

    added a similar users page that shows all matched users, you can click on every cell to see what exactly you matched on made the groups page look better and improved overall UI

--- back_end/db/schema.rb
+++ back_end/db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_204640) do
+ActiveRecord::Schema.define(version: 2019_11_28_011437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
   create_table "gros", force: :cascade do |t|
@@ -62,6 +63,11 @@ ActiveRecord::Schema.define(version: 2019_11_15_204640) do
   create_table "user_preferences", force: :cascade do |t|
     t.string "name"
     t.string "interest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_profiles", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
