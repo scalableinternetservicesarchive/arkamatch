5e8eedf995411ce0357e2fdacf0dd437a53ed1bc
Hakan Alpay <hak7alp@gmail.com>
2019-11-29 23:26:50 -0800

    removed jquery from application.js's and Gemfile

--- back_end/app/javascript/packs/application.js
+++ back_end/app/javascript/packs/application.js
@@ -3,10 +3,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -16,9 +12,3 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-// require("rails-ujs")
-// require("activestorage")
-require("jquery")
-require("popper")
-require("bootstrap-sprockets")
-// require_tree .
