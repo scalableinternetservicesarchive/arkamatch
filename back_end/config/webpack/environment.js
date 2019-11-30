ad21a1280498d2aa131349f2d4a2405b3d334704
Yijing Zhou <yijingz1227@gmail.com>
2019-11-30 14:15:27 -0800

    Still getting popper error; reducing the variable to test jquery fix

--- back_end/config/webpack/environment.js
+++ back_end/config/webpack/environment.js
@@ -2,8 +2,8 @@ const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
     $: 'jquery',
-    jQuery: 'jquery',
-    jquery: 'jquery',
+    jQuery: 'jquery/src/jquery',
+    jquery: 'jquery/src/jquery',
     Popper: ['popper.js', 'default'],
     popper: ['popper.js', 'default']
 }))
