28cf32bfc49b2a853b6bebbe88b894414634990d
Yijing Zhou <yijingz1227@gmail.com>
2019-11-30 14:40:08 -0800

    Revert "require Bootstrap in application.js"
    This reverts commit 7ff574b3a25c5d43cbb4f2f652392005a2b06094.

--- front_end/app/controllers/sessions_controller.rb
+++ front_end/app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-    puts params.inspect
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
