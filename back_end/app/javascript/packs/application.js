7ff574b3a25c5d43cbb4f2f652392005a2b06094
Yijing Zhou <yijingz1227@gmail.com>
2019-11-30 14:40:08 -0800

    require Bootstrap in application.js
    From the look of it, there should be no need to require popper explicitly in application.js since the tutorial we followed aims to integrate Bootstrap ad the end goal

--- back_end/app/javascript/packs/application.js
+++ back_end/app/javascript/packs/application.js
@@ -3,10 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -18,5 +18,6 @@ require("channels")
 
 // require("rails-ujs")
 // require("activestorage")
-require("jquery")
+require('bootstrap');
+require("jquery");
 // require_tree .
--- front_end/app/controllers/sessions_controller.rb
+++ front_end/app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+    puts params.inspect
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
