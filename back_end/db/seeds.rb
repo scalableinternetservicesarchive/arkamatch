a6562609b922657b767695e2b3cd1d3f661c8462
mertdusunceli <mertdusunceli@gmail.com>
2019-12-04 01:34:29 -0800

    sql to add new users to db users

--- back_end/db/seeds.rb
+++ back_end/db/seeds.rb
@@ -11,17 +11,29 @@ number_of_interests = [2,3,4]
 require 'csv'
 csv_text = File.read(Rails.root.join('lib','seeds', 'users_sample.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-csv.each do | row |
-  user = User.create! :username => row['username'],
-                     :email => row['email_prefix']+"@bogus.com",
-                      :password =>  row['password'],
-                      :password_confirmation => row['password']
+timeNow = Time.now
+sql = "INSERT INTO users (username, email, encrypted_password, created_at, updated_at) VALUES "
+csv.each_with_index do | row, index | 
+  if index == 0
+    sql += "( '#{row['username']}', '#{row['email_prefix']}@bogus.com', '#{row['password']}', '#{timeNow}', '#{timeNow}')"  
+  else
+    sql += ",( '#{row['username']}', '#{row['email_prefix']}@bogus.com', '#{row['password']}', '#{timeNow}', '#{timeNow}')"
+  end 
+end 
+sql += ';'
+# puts sql 
+results = ActiveRecord::Base.connection.execute(sql)
+puts results
+# csv.each do | row |
+#   user = User.create! :username => row['username'],
+#                      :email => row['email_prefix']+"@bogus.com",
+#                       :password =>  row['password'],
+#                       :password_confirmation => row['password']
 
-  if row['username'] != "system_admin"
-    random_interests = interests.sample(number_of_interests.sample(1)[0])
-    random_interests.each do |interest|
-      preference = UserPreference.create! :name => row['username'], :interest => interest
-    end
-  end
-
-end
+#   if row['username'] != "system_admin"
+#     random_interests = interests.sample(number_of_interests.sample(1)[0])
+#     random_interests.each do |interest|
+#       preference = UserPreference.create! :name => row['username'], :interest => interest
+#     end
+#   end
+# end
