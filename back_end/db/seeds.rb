b5822f18e510d2a8a4c0c39dd6a532ffb1a6c1fc
Yijing Zhou <yijingz1227@gmail.com>
2019-12-04 14:51:27 -0800

    Revert "SQL statements to make `db:seed` faster"

--- back_end/db/seeds.rb
+++ back_end/db/seeds.rb
@@ -11,43 +11,17 @@ number_of_interests = [2,3,4]
 require 'csv'
 csv_text = File.read(Rails.root.join('lib','seeds', 'users_sample.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do | row |
+  user = User.create! :username => row['username'],
+                     :email => row['email_prefix']+"@bogus.com",
+                      :password =>  row['password'],
+                      :password_confirmation => row['password']
 
-timeNow = Time.now
-sql = "INSERT INTO users (username, email, encrypted_password, created_at, updated_at) VALUES "
-preferenceSql = "INSERT INTO user_preferences (name, interest, created_at, updated_at) VALUES"
-
-csv.each_with_index do | row, index | 
-  if index == 0   # exclude admin from having interests 
-    sql += "('#{row['username']}', '#{row['email_prefix']}@bogus.com', '#{row['password']}', '#{timeNow}', '#{timeNow}')"  
-  else
-    sql += ",('#{row['username']}', '#{row['email_prefix']}@bogus.com', '#{row['password']}', '#{timeNow}', '#{timeNow}')"
+  if row['username'] != "system_admin"
     random_interests = interests.sample(number_of_interests.sample(1)[0])
-    
     random_interests.each do |interest|
-      preferenceSql += " ('#{row['username']}', '#{interest}', '#{timeNow}', '#{timeNow}'),"  
+      preference = UserPreference.create! :name => row['username'], :interest => interest
     end
-  end 
-end 
-sql += ';'
-preferenceSql.delete_suffix!(',')
-preferenceSql += ';'
-# Builds up both sql queries and runs them at once 
-results = ActiveRecord::Base.connection.execute(sql)
-preferenceResults = ActiveRecord::Base.connection.execute(preferenceSql)
+  end
 
-
-
-# csv.each do | row |
-#   user = User.create! :username => row['username'],
-#                      :email => row['email_prefix']+"@bogus.com",
-#                       :password =>  row['password'],
-#                       :password_confirmation => row['password']
-
-#   if row['username'] != "system_admin"
-#     random_interests = interests.sample(number_of_interests.sample(1)[0])
-#     random_interests.each do |interest|
-#       preference = UserPreference.create! :name => row['username'], :interest => interest
-#     end
-#   end
-
-# end
+end
